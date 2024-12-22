90762436a75d9a3aef1cc40e709c7bfe02d769a2
ayush kumar <ayush2422005@gmail.com>
2024-12-22 19:42:28 +0530

    entrprize update

--- app/admin/marketing/page.tsx
+++ app/admin/marketing/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState, useRef } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { FiEdit2, FiTrash2, FiEye, FiEyeOff } from 'react-icons/fi';
 
 interface MarketingBanner {
   _id: string;
