43f25aeb0611cbe297cddf530d2bb67997de796b
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2025-11-11 00:54:01 +0530

    meta pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -293,6 +293,23 @@ export default function RootLayout({
             })
           }}
         />
+
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '956614123206448');
+fbq('track', 'PageView');`}
+        </Script>
+
+        {/* noscript fallback for users with JS disabled */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=956614123206448&ev=PageView&noscript=1" />` }} />
       </body>
     </html>
   )
